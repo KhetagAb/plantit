81ab579d4e916db72f92f3259ff141cb2c2e1140
Khetag Dzestelov <dzkhetag@gmail.com>
2023-02-19 18:59:35 +0300

    Init commit

---
+++ rancher-service/src/test/java/ru/tinkoff/rancherservice/RancherServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tinkoff.rancherservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RancherServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
